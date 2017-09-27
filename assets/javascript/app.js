0daff3b0c62bc505e74630a5a9cb8b243783a304
JC Scalabre <jc_scalabre@hotmail.com>
2017-09-27 17:25:33 -0500

    wed 5:25pm

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,21 @@
 var question1 = {
 	questionNumber: "Question 1",
 	question: "What is the capital of France?",
-	answers: ["Bordeaux", "Lyon", "Marseille", "Paris"],
+	answers: ["Bordeaux", "Paris", "Marseille", "Lyon"],
 	rightanswer: "Paris"
+}
+
+var question2 = {
+	questionNumber: "Question 2",
+	question: "What is the capital of Australia?",
+	answers: ["Sydney", "Melbourne", "Perth", "Canberra"],
+	rightanswer: "Canberra"
 }
 
 // HTML for 'questionpage' page:
 var questionpage = '<div class="container"><div class="row"><div class="col-md-2 mx-auto" id="questionNumber">Question 1</div></div><div class="row"><div class="col-md-6 mx-auto" id="question">Who was the 33rd president of fdsa fdsa fdsa fdsaf safdsa the USA?</div></div><div class="row"><div class="col-md-4 mx-auto" id="timeleft">Time left: <span id="timeleftnumber">30</span></div></div></div><div class="list-group mx-auto"><button href="#" class="list-group-item list-group-item-action" id="question1">Answer 1</button><button href="#" class="list-group-item list-group-item-action" id="question2">Answer 2</button><button href="#" class="list-group-item list-group-item-action" id="question3">Dapibus ac facilisis in</button><button href="#" class="list-group-item list-group-item-action" id="question4">Dapibus ac facilisis in</button></div>';
-var time = 10;
+var time = 5;
+var timevar;
 
 // When the start button is clicked:
 $("#startbutton").on("click", start);
@@ -19,13 +27,10 @@ $("#startbutton").on("click", start);
 
 // Functions: 
 
-
-
-
 // Our start function that executes when we click the start button:
 function start() {
 	$("body").html(questionpage);
-	setInterval(timer, 1000);
+	timervar = setInterval(timer, 1000);
 	setquestion();
 
 	// When you click Answer 1:
@@ -40,9 +45,8 @@ function start() {
 }
 
 // Our correct answer checker for when we click an answer:
-
 function clickanswerdetector() {
-		if($(this).html() === question1.rightanswer) {
+		if($(this).html() === (question1).rightanswer || $(this).html() === (question2).rightanswer) {
 			console.log("You chose the right answer. Good job!!!");
 		} else {
 			console.log("You chose the wrong answer :(")
@@ -57,12 +61,9 @@ function timer() {
 	$("#timeleftnumber").html(time);
 	console.log(time);
 
-
-
 	// When time is up:
 	if (time === 0) {
-		$("#timeleft").html("Time is up! <br> The right answer was: ")
-		$("#rightanswer").css("background-color", "green");
+		clearInterval(timervar);
 		answerdetector();
 	}
 }
@@ -76,7 +77,12 @@ function setquestion() {
 	}
 }
 
+// This function checks what the right answer was:
 function answerdetector() {
+	
+	// Changes the timer section to a message saying time is up:
+	$("#timeleft").html("Time is up! <br> The right answer was: ")
+
 	// This for loop detects the right answer and highlights it in green:
 	for (var i = 1; i < question1.answers.length + 1; i++) {
 		if (question1.answers[i-1] === question1.rightanswer) {
