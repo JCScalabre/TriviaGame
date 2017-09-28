dde9eb05bc6e82446ff12a098eacea34f7a85c8a
JC Scalabre <jc_scalabre@hotmail.com>
2017-09-28 16:31:44 -0500

    removed second js file (old one)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,170 +1,177 @@
-// Variables:
+// Questions array:
+questions = [
 
-// Questions:
-var question1 = {
+question1 = {
 	questionNumber: "Question 1",
 	question: "What is the capital of France?",
 	answers: ["Bordeaux", "Paris", "Marseille", "Lyon"],
-	rightanswer: "Paris"
-}
-
-var question2 = {
+	rightanswer: "Paris",
+},
+question2 = {
 	questionNumber: "Question 2",
 	question: "What is the capital of Australia?",
 	answers: ["Sydney", "Melbourne", "Perth", "Canberra"],
-	rightanswer: "Canberra"
+	rightanswer: "Canberra",
+},
+question3 = {
+	questionNumber: "Question 3",
+	question: "What is the capital of Germany?",
+	answers: ["Berlin", "Hamburg", "Munich", "Frankfurt"],
+	rightanswer: "Berlin",
+},
+question4 = {
+	questionNumber: "Question 4",
+	question: "What is the capital of India?",
+	answers: ["Mumbai", "Kolkata", "New Delhi", "Kanpur"],
+	rightanswer: "New Delhi",
 }
 
-// HTML for 'questionpage' page:
-var questionpage = '<div class="container"><div class="row"><div class="col-md-2 mx-auto" id="questionNumber">Question 1</div></div><div class="row"><div class="col-md-6 mx-auto" id="question">Who was the 33rd president of fdsa fdsa fdsa fdsaf safdsa the USA?</div></div><div class="row"><div class="col-md-6 mx-auto" id="timeleft">Time left: <span id="timeleftnumber">30</span></div></div></div><div class="list-group mx-auto"><button href="#" class="list-group-item list-group-item-action" id="question1">Answer 1</button><button href="#" class="list-group-item list-group-item-action" id="question2">Answer 2</button><button href="#" class="list-group-item list-group-item-action" id="question3">Dapibus ac facilisis in</button><button href="#" class="list-group-item list-group-item-action" id="question4">Dapibus ac facilisis in</button></div>';
+]
+
+// console.log(questions[0].rightanswer);
+// console.log(questions[0].answers[0]);
+
+// Variables: 
+
+var correctanswers = 0;
+var incorrectanswers = 0;
+var unanswered = 0;
 var time = 30;
-var nexttime = 3;
-var timervar;
-var lockstate = false;
-
-// Main code: 
-
-// When the start button is clicked:
-$("#startbutton").on("click", start);
+var timevar;
+var currentquestion = 0;
 
 
-// Functions: 
 
-// Our start function that executes when we click the start button:
-function start() {
-	$("body").html(questionpage);
-	timervar = setInterval(timer, 1000);
-	setquestion1();
-
-	console.log(lockstate);
-
-	// When you click Answer 1:
-	$("#question1").on("click", clickanswer1detector);
-
-	$("#question2").on("click", clickanswer1detector);
-	
-	$("#question3").on("click", clickanswer1detector);
-
-	$("#question4").on("click", clickanswer1detector);
-}
-
-
-// Our timer function:
 function timer() {
 	time--;
-
-	// This pushes our time variable into the time DOM:
+	// console.log(time);
 	$("#timeleftnumber").html(time);
-	console.log(time);
-
 	// When time is up:
 	if (time === 0) {
-		clearInterval(timervar);
-		console.log("Time is up");
-		answer1detector();
-	}
-}
+		console.log("Time is up!");
+		unanswered++;
+		$("#timeleft").html("Time is up! The correct answer was: ");
 
-// Question 1 functions:
+		// This loop goes through the 4 answers:
+		for ( i = 0; i < questions[currentquestion].answers.length; i++) {
+			// This answer variable is the contents of the answer button
+			var answer = $("#question"+(i)).html();
 
-// Pushes question into our DOM:
-function setquestion1() {
-	$("#questionNumber").html(question1.questionNumber);
-	$("#question").html(question1.question);
-	for (var i = 1; i < question1.answers.length + 1; i++) {
-		$("#question" + i).html(question1.answers[i-1]);
-	}
-}
-
-// Our correct answer checker for when we click an answer:
-function clickanswer1detector() {
-
-	if (lockstate === false) {
-		if($(this).html() === (question1).rightanswer) {
-			nexttime=4;
-			timervar2 = setInterval(function() {
-				nexttime--;
-				console.log(nexttime);
-				// Pushing time var into timeleftnumber span
-				$("#nextleftnumber").html(nexttime);
-				if (nexttime===0){
-					console.log("Moving onto question 2");
-					setquestion2();
-					clearInterval(timervar2);
-					lockstate=false;
-					clickanswer2detector();
+			if (answer === questions[currentquestion].rightanswer) {
+				// Makes the right answer highlighted in green:
+				$("#question"+i).css("background-color", "lightgreen");
+				// Sets the next question after 3 seconds:
+				if (currentquestion === 3) {
+					clearInterval(timevar);
+					setTimeout(scorepage, 4000);
+				} else {
+					setTimeout(setquestion, 4000);
+					currentquestion++;
 				}
-			}, 1000);
+			}
+		}
+	}
+}
 
-			console.log("You chose the right answer. Good job!!!");
-			// Replacing timeleft with congrats message:
-			$("#timeleft").html("Congratulations! <br> That was the right answer. <br> Next question starts in: <span id='nextleftnumber'>3</span>");
-			
-			clearInterval(timervar);
-			$(this).css("background-color", "green");
+function setquestion() {
+	time = 30;
+	
+	questionpage();
+
+	$(".list-group-item").on("click", answerdetector);
+
+	$("#questionNumber").html(questions[currentquestion].questionNumber);
+	$("#question").html(questions[currentquestion].question);
+	$("#timeleft").html("Time left: <span id='timeleftnumber'>30</span>")
+	for (j = 0 ; j <= questions[currentquestion].answers.length-1; j++) {
+		$("#question" + j).html(questions[currentquestion].answers[j]);
+	}
+	// For loop that clears css from all answers:
+	for ( i = 0; i < questions[currentquestion].answers.length; i++) {
+		$("#question"+i).css("background-color", "white");
+
+	}
+}
+
+function answerdetector() {
+	console.log("Current Question #: " + currentquestion);
+
+	// If you press the right answer:
+	if ($(this).html() === questions[currentquestion].rightanswer) {
+		console.log("You pressed the right answer");
+		correctanswers++;
+		$(this).css("background-color", "lightgreen");
+		$("#timeleft").html("Congratulations! That was the correct answer");
+		if (currentquestion !== 3) {
+			setTimeout(setquestion, 4000);
+			currentquestion++;
+			time = 30;
 		} else {
-			console.log("You chose the wrong answer :(");
-			// Replacing timeleft with oh no message:
-			$("#timeleft").html("Oh no! <br> That was the wrong answer.")
-			
-			clearInterval(timervar);
-			$(this).css("background-color", "red");
-			$("#question2").css("background-color", "green");
-		}	
-	}
-	lockstate = true;
-}
+			clearInterval(timevar);
+			setTimeout(scorepage, 4000);
+		}
+	} 
 
-// This function checks what the right answer was:
-function answer1detector() {
-	
-	// Changes the timer section to a message saying time is up:
-	$("#timeleft").html("Time is up! <br> The right answer was: ")
+	// If you press the wrong answer:
+	else {
+		console.log("You pressed the wrong answer");
+		incorrectanswers++;
+		$("#timeleft").html("Oh no! That was the wrong answer");
 
-	// This for loop detects the right answer and highlights it in green:
-	for (var i = 1; i < question1.answers.length + 1; i++) {
-		if (question1.answers[i-1] === question1.rightanswer) {
-			$("#question" + i).css("background-color", "green");
-		} 	
-	}	
-}
+		// For loop to detect what the right answer was and highlight it in green:
+		for ( i = 0; i < questions[currentquestion].answers.length; i++) {
+			// This answer variable is the contents of the answer button
+			var answer = $("#question"+(i)).html();
 
-// Question 2 functions:
+			if (answer === questions[currentquestion].rightanswer) {
+				// Makes the right answer highlighted in green:
+				$("#question"+i).css("background-color", "lightgreen");
+			}
+		}
 
-// Pushes question into our DOM:
-function setquestion2() {
-	$("#questionNumber").html(question2.questionNumber);
-	$("#question").html(question2.question);
-	for (var i = 1; i < question2.answers.length + 1; i++) {
-		$("#question" + i).html(question2.answers[i-1]);
+		$(this).css("background-color", "tomato");
+		if (currentquestion !== 3) {
+			setTimeout(setquestion, 4000);
+			currentquestion++;
+			time = 30;
+		} else {
+			clearInterval(timevar);
+			setTimeout(scorepage, 4000);
+		}
 	}
 }
 
-// Our correct answer checker for when we click an answer:
-function clickanswer2detector() {
-	if($(this).html() === (question2).rightanswer) {
-		console.log("You chose the right answer. Good job!!!");
-	} else {
-		console.log("You chose the wrong answer :(")
-	}	
+function questionpage() {
+	var questionpage = '<div class="container"><div class="row"><div class="col-md-4 mx-auto" id="questionNumber">Question 0</div></div><div class="row"><div class="col-md-6 mx-auto" id="question">What is the capital of Korea?</div></div><div class="row"><div class="col-md-4 mx-auto" id="timeleft">Time left: <span id="timeleftnumber">30</span></div></div></div><div class="list-group mx-auto"><button href="#" class="list-group-item list-group-item-action" id="question0">1111</button><button href="#" class="list-group-item list-group-item-action" id="question1">2222</button><button href="#" class="list-group-item list-group-item-action" id="question2">3333</button><button href="#" class="list-group-item list-group-item-action" id="question3">4444</button></div>';
+	$("body").html(questionpage);
 }
 
-// This function checks what the right answer was:
-function answer2detector() {
-	
-	// Changes the timer section to a message saying time is up:
-	$("#timeleft").html("Time is up! <br> The right answer was: ")
+// Sets the page to the score page:
+function scorepage() {
+	var scorepage = '<div class="card mx-auto"><h4>Results</h4><p></p><p>Correct answers: <span id="correct"></span></p><p>Incorrect answers: <span id="incorrect"></span></p><p>Unanswered questions: <span id="unanswered"></span></p><button class="btn btn-primary mx-auto" id="playagain">Play Again</button></div>'
+	currentquestion = 0;
+	$("body").html(scorepage);
 
-	// This for loop detects the right answer and highlights it in green:
-	for (var i = 1; i < question2.answers.length + 1; i++) {
-		if (question2.answers[i-1] === question2.rightanswer) {
-			$("#question" + i).css("background-color", "green");
-		} 	
-	}	
+	$("#correct").html(correctanswers);
+	$("#incorrect").html(incorrectanswers);
+	$("#unanswered").html(unanswered);
+
+	$("#playagain").on("click", function() {
+		$("body").html('<div class="card mx-auto"><h4>Trivia Game</h4><p>Welcome to the Trivia Quiz, press start to begin!</p><button class="btn btn-primary mx-auto" id="startbutton">Start</button></div>');
+		correctanswers = 0;
+		incorrectanswers = 0;
+		unanswered = 0;
+		$("#startbutton").on("click", function() {
+			timevar = setInterval(timer, 1000);
+			setquestion();
+		});
+	});
 }
 
-function answerpage() {
+// Main code:
 
-}
-
-start();
+// When the start button is clicked:
+$("#startbutton").on("click", function() {
+	timevar = setInterval(timer, 1000);
+	setquestion();
+});
